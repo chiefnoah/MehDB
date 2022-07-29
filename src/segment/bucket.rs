00adb793e2ab49f055f307665ce786475a747085
Noah Pederson <noah@packetlost.dev>
2022-07-28 19:05:39 -0500

    Adds bucket tests, bucket is broken, fix it

--- src/segment/bucket.rs
+++ src/segment/bucket.rs
@@ -97,7 +97,7 @@ mod test {
     use io::{self, Read, Write, Cursor, Seek};
     
     #[test]
-    fn test_bucket_pack() {
+    fn bucket_can_pack() {
         let mut bucket = Bucket {offset: 5, buf: [0; BUCKET_SIZE]};
         // change this so we have something to check for
         bucket.buf[0] = 255;
@@ -108,9 +108,64 @@ mod test {
             Ok(r) => r,
         };
         let mut expected_buf: [u8; BUCKET_SIZE + 5] = [0; BUCKET_SIZE + 5];
+        // this will only match up with the output buffer if the Seek is performed
+        // propery
         expected_buf[5] = 255;
         let inner_buf = buf.into_inner();
         assert_eq!(&inner_buf.len(), &(BUCKET_SIZE + bucket.offset as usize));
         assert_eq!(&expected_buf[..], &inner_buf[..]);
+    }
+
+    #[test]
+    fn bucket_can_unpack() {
+        let mut fixture: Vec<u8> = Vec::from([0; 2 * BUCKET_SIZE + 5]);
+        fixture[1] = 0x12;
+        let mut buf = Cursor::new(fixture);
+        buf.seek(io::SeekFrom::Start(1)).unwrap();
+        let bucket = match Bucket::unpack(&mut buf) {
+            Err(e) => panic!("Unable to unpack Bucket: {}", e),
+            Ok(h) => h,
+        };
+        assert_eq!(bucket.offset, 1);
+        assert_eq!(bucket.buf[0], 0x12);
+    }
+
+    #[test]
+    fn when_buffer_is_too_small_header_unpack_fails() {
+        // Too small!
+        let mut fixture: Vec<u8> = Vec::from([0; 16]);
+        fixture[1] = 0x12;
+        let mut buf = Cursor::new(fixture);
+        buf.seek(io::SeekFrom::Start(1)).unwrap();
+        match Bucket::unpack(&mut buf) {
+            Err(e) => (),
+            Ok(h) => panic!("Improperly was able to unpack Bucket"),
+        }
+    }
+
+    #[ignore]
+    #[test]
+    fn can_put_and_get_records_from_bucket() {
+        let mut bucket = Bucket {offset: 0, buf: [0; BUCKET_SIZE ]};
+        let test_record1 = Record{hash_key: 12345, value: 666};
+        for i in 1..=16 {
+            let res = match bucket.put(i * 60, i * 2, 0) {
+                Err(e) => panic!("Unable to insert record: {}", e),
+                Ok(o) => o,
+            };
+        }
+        // Bucket overflow
+        //match bucket.put(1234, 666, 0) {
+        //    Ok(_) => panic!("Bucket should have overflown, but didn't"),
+        //    Err(e) => (),
+        //}
+        for i in 1..=16 {
+            let record = match bucket.get(i * 60) {
+                None => panic!("Unable to fetch record from bucket"),
+                Some(r) => r,
+            };
+            assert_eq!(record.hash_key, i * 60);
+            assert_eq!(record.value, i * 2);
+        }
     }
 }
